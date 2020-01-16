416464c06b3fa7dcaf1da620a8a601e1b5307afa
Javier Ocampo <ferroxido@gmail.com>
2020-01-15 21:39:32 -0300

    WIP: 4inline game

--- 4inline.py
+++ 4inline.py
@@ -1,7 +1,41 @@
 import numpy as np
 
-def create_board():
-    return np.zeros((6, 7))
+ROWS = 6
+COLUMNS = 7
 
-board = create_board()
-print(board)
+
+def create_board():
+    return np.zeros((ROWS, COLUMNS))
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_location(board, col):
+    return board[0][col] == 0
+
+def get_next_free_row(board, col):
+    for r in reversed(range(board.shape[0])):
+        if board[r][col] == 0:
+            return r
+
+def main():
+    board = create_board()
+    game_over = False
+    turn = 0
+    while not game_over:
+        if turn == 0:
+            col = int(input('Player 1 Make your selection (0-6):'))
+            if is_valid_location(board, col):
+                row = get_next_free_row(board, col)
+                drop_piece(board, row, col, 1)
+        else:
+            col = int(input('Player 2 Make your selection (0-6):'))
+            if is_valid_location(board, col):
+                row = get_next_free_row(board, col)
+                drop_piece(board, row, col, 2)
+
+        print(board)
+        turn += 1
+        turn = turn % 2
+
+main()
---
+++ README.md
@@ -0,0 +1,9 @@
+# Python games
+
+Serie of games to learn more about python language.
+
+## Games
+
+1. PONG
+2. SNAKE
+3. 4 IN LINE
