205ec30c5f944f47a0c7b864b37f0ef041322db1
Javier Ocampo <ferroxido@gmail.com>
2020-01-23 09:50:42 -0300

    WIP Tetris game: Draw window stuffs

--- Tetris.py
+++ Tetris.py
@@ -8,6 +8,7 @@ represented in order by 0 - 6
 """
 
 pygame.font.init()
+font = pygame.font.SysFont('comicsans', 60)
 
 # GLOBALS VARS
 s_width = 800
@@ -19,6 +20,7 @@ block_size = 30
 top_left_x = (s_width - play_width) // 2
 top_left_y = s_height - play_height
 
+window = pygame.display.set_mode((s_width, s_height))
 
 # SHAPE FORMATS
 S = [['.....',
@@ -134,9 +136,18 @@ class Piece(object):
         self.y = y
         self.shape = shape
         self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
+
 
 def create_grid(locked_positions={}):
-    pass
+    grid = [[(0,0,0) for _ in range(10)] for _ in range(20)]
+
+    for i in range(len(grid)):
+      for j in range(len(grid[i])):
+        if (j,i) in locked_positions:
+          grid[i][j] = locked_positions[(j,i)]
+
+    return grid
 
 def convert_shape_format(shape):
     pass
@@ -148,13 +159,20 @@ def check_lost(positions):
     pass
 
 def get_shape():
-    pass
+    return random.choice(shapes)
 
 def draw_text_middle(text, size, color, surface):
     pass
 
-def draw_grid(surface, row, col):
-    pass
+def draw_grid(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            # draw.rect(window, color, (x,y,w,h))
+            pygame.draw.rect(window, grid[i][j], (top_left_x + j*block_size, top_left_y + i*block_size, block_size, block_size))
+
+    # grid border
+    pygame.draw.rect(window, (255,255,255), (top_left_x, top_left_y, play_width, play_height), 4)
+    # pygame.display.update()
 
 def clear_rows(grid, locked):
     pass
@@ -162,13 +180,21 @@ def clear_rows(grid, locked):
 def draw_next_shape(shape, surface):
     pass
 
-def draw_window(surface):
-    pass
+def draw_window(grid):
+    window.fill((0,0,0))
+    label = font.render('Tetris', 1, (255,255,255))
+    window.blit(label, (s_width/2 - label.get_width()/2, 30))
+    draw_grid(grid)
+    pygame.display.update()
 
 def main():
-    pass
+    grid = create_grid()
+    while True:
+        draw_window(grid)
+
 
 def main_menu():
     pass
 
-main_menu()  # start game
+# main_menu()  # start game
+main()
